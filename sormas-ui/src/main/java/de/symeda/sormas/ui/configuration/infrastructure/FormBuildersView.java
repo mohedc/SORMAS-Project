32fd7981e5e89d2cd3f3760428343cdd998f2c99
Flava177 <flavaflav177@gmail.com>
2026-05-19 16:01:10 +0000

    Restored Initial form fields visibility

--- sormas-ui/src/main/java/de/symeda/sormas/ui/configuration/infrastructure/FormBuildersView.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/configuration/infrastructure/FormBuildersView.java
@@ -83,8 +83,7 @@ public class FormBuildersView extends AbstractConfigurationView {
 
 	private static final long serialVersionUID = 1L;
 
-//	public static final String VIEW_NAME = ROOT_VIEW_NAME + "/formBuilders-md-fhgfj-awaiting";
-	public static final String VIEW_NAME = ROOT_VIEW_NAME + "/formBuilders";
+	public static final String VIEW_NAME = ROOT_VIEW_NAME + "/formBuilders-md-fhgfj-awaiting";
 
 	private FormBuilderCriteria criteria;
 	private ViewConfiguration viewConfiguration;
